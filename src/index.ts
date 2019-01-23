d34d92c5101e5f7bc14c8035c07c32bebf8b89d5
Bradley Bossard <bradleybossard@gmail.com>
2019-01-23 17:36:24 -0600

    Add get route for rootl

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,7 @@ import * as express from "express"
 
 const app = express()
 
+
+app.get('/', (req, res) => res.send("Welcome home"))
+
 app.listen(8000, () => console.log("Server running on 8000!"))
